2a02d4a62044eed1024b9a7795dbe2f6816c2915
Kamil Kucharski <kamil.kucharski@makimo.pl>
2019-12-18 15:00:45 +0100

    Add base dev config

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="app">
+        <p class="hello-world">Hello world</p>
+        <img src="./assets/images/download.png">
+        <router-view/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
