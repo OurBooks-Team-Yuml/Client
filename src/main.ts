f7fd2b6f2841f65463a036723239c15b466c79cd
Kamil Supera <kamil.supera@makimo.pl>
2020-06-12 11:16:23 +0200

    Add base setup for vuex

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue';
-import App from './App.vue'
 import router from './router'
+import store from './store'
+import App from './App.vue'
+
 
 const app = createApp(App);
 
 app.use(router);
+app.use(store);
 
 router.isReady().then(() => {
     app.mount('#app');
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+
+import actions from './actions'
+import getters from './getters'
+import mutations from './mutations'
+import state from './state'
+
+export default createStore({
+    actions,
+    getters,
+    mutations,
+    state
+})
+
+
